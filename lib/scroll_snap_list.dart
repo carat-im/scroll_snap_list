d1811c0b2d29ef6978051a77295a5727de61eac1
Hansol Lee <giantsol64@gmail.com>
2021-11-03 00:10:35 +0900

    스스로 dispose하지 않도록 수정

--- lib/scroll_snap_list.dart
+++ lib/scroll_snap_list.dart
@@ -298,12 +298,6 @@ class ScrollSnapListState extends State<ScrollSnapList> {
   }
 
   @override
-  void dispose() {
-    widget.listController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Container(
       padding: widget.padding,
